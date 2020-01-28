90307b935fb5743e9061d7e40e68f09d8e90ebe8
rogerio-romao <djgroovy@gmail.com>
2020-01-28 04:16:29 +0000

    Added models for user, review and post

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    // username and password not added because they will come from passport by default
+    email: String,
+    image: String,
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    }]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
+
+// User:
+// - email - string
+// - password - string
+// - username - string
+// - profilePic - string
+// - posts - array of objects ref Post
+// - reviews - array of objects ref Review
