968141c49a2c6a78bc5607fcfbefea07eb7d69ad
rogerio-romao <djgroovy@gmail.com>
2020-02-16 22:52:47 +0000

    cluster maps finished

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,7 +16,8 @@ module.exports = {
     async postIndex(req, res, next) {
         let posts = await Post.paginate({}, {
             page: req.query.page || 1,
-            limit: 10
+            limit: 10,
+            sort: { '_id': -1 }
         });
         posts.page = Number(posts.page);
         res.render('posts/index', { posts, mapBoxToken, title: 'Posts Index' });
@@ -42,7 +43,7 @@ module.exports = {
                 limit: 1
             })
             .send();
-        req.body.post.coordinates = response.body.features[0].geometry.coordinates;
+        req.body.post.geometry = response.body.features[0].geometry;
         let post = new Post(req.body.post);
         post.properties.description = `<strong><a href="/posts/${post._id}">${post.title}</a></strong><p>${post.location}</p><p>${post.description.substring(0, 20)}...</p>`;
         await post.save();
@@ -104,13 +105,14 @@ module.exports = {
                     limit: 1
                 })
                 .send();
-            post.coordinates = response.body.features[0].geometry.coordinates;
+            post.geometry = response.body.features[0].geometry;
             post.location = req.body.post.location;
         }
         // update the post with any new properties
         post.title = req.body.post.title;
         post.description = req.body.post.description;
         post.price = req.body.post.price;
+        post.properties.description = `<strong><a href="/posts/${post._id}">${post.title}</a></strong><p>${post.location}</p><p>${post.description.substring(0, 20)}...</p>`;
         //save to db then redirect
         post.save();
         res.redirect(`/posts/${post.id}`);
