b300a3c369dccf28e74316ebe5c30ef325929935
rogerio-romao <djgroovy@gmail.com>
2020-02-18 14:41:31 +0000

    merged with latest

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -1,16 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9nZXJpby1yb21hbyIsImEiOiJjazY4N2d1a3YwMHBxM2huOWdsZ3BucWE4In0.KVIhyqaSOLMCjKpRyBG8XA';
 
-<<<<<<< HEAD
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
-    center: post.coordinates,
-=======
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: post.geometry.coordinates,
->>>>>>> multer-cloudinary
     zoom: 5
 });
 
@@ -20,12 +13,6 @@ let el = document.createElement('div');
 el.className = 'marker';
 // make a marker for our location and add to the map
 new mapboxgl.Marker(el)
-<<<<<<< HEAD
-    .setLngLat(post.coordinates)
-    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-        .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
-    .addTo(map);
-=======
     .setLngLat(post.geometry.coordinates)
     .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
         .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
@@ -44,4 +31,3 @@ $('.toggle-edit-form').on('click', function() {
 $('.clear-rating').click(function() {
     $(this).siblings('.input-no-rate').click();
 });
->>>>>>> multer-cloudinary
